a8d515f8a6972cf6a3af2a5f1eac5d200b617da3
kaitoseita <leadik2468@gmail.com>
2023-10-24 16:40:10 +0900

    Initial Commit

---
+++ NavigationBar/NavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBarApp.swift
+//  NavigationBar
+//
+//  Created by セイタカイト on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
