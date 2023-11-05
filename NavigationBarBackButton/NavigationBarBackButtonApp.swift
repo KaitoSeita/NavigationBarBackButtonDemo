188fd41b11838a85d6c04c70f47f0eea1a25b3d8
kaitoseita <leadik2468@gmail.com>
2023-11-05 14:41:43 +0900

    Appファイル

---
+++ NavigationBarBackButton/NavigationBarBackButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBarBackButtonApp.swift
+//  NavigationBarBackButton
+//
+//  Created by kaito-seita on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBarBackButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FirstView()
+        }
+    }
+}
